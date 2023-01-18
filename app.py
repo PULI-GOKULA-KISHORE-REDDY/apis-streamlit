771a7e29b21fcb674382b84ea31901098664525e
PULI GOKULA KISHORE REDDY <94065306+PULI-GOKULA-KISHORE-REDDY@users.noreply.github.com>
2023-01-18 14:40:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,28 @@
-import uuid
+import streamlit as st
 
-import cv2
-import uvicorn
-from fastapi import File
-from fastapi import FastAPI
-from fastapi import UploadFile
-import numpy as np
-from PIL import Image
+def main():
+    # Register your pages
+    pages = {
+        "First page": page_first,
+        "Second page": page_second,
+    }
 
-import config
-import inference
+    st.sidebar.title("App with pages")
 
+    # Widget to select your page, you can choose between radio buttons or a selectbox
+    page = st.sidebar.radio("Select your page", tuple(pages.keys()))
+    #page = st.sidebar.selectbox("Select your page", tuple(pages.keys()))
 
-app = FastAPI()
+    # Display the selected page
+    pages[page]()
 
+def page_first():
+    st.title("This is my first page")
+    # ...
 
-@app.get("/")
-def read_root():
-    return {"message": "Welcome from the API"}
-
-
-@app.post("/{style}")
-def get_image(style: str, file: UploadFile = File(...)):
-    image = np.array(Image.open(file.file))
-    model = config.STYLES[style]
-    output, resized = inference.inference(model, image)
-    name = f"/storage/{str(uuid.uuid4())}.jpg"
-    cv2.imwrite(name, output)
-    return {"name": name}
-
+def page_second():
+    st.title("This is my second page")
+    # ...
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8080)
+    main()
